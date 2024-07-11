b101fc117c4f161f78c1783391f8719c619d15a5
Liam Dyer <liamcdyer@gmail.com>
2024-07-11 16:54:44 -0400

    feat: initial configuration support

---
+++ lua/blink/cmp/config.lua
@@ -0,0 +1,48 @@
+local M = {}
+
+M.default = {
+  kind_icons = {
+    Text = '󰉿',
+    Method = '󰊕',
+    Function = '󰊕',
+    Constructor = '󰒓',
+
+    Field = '󰜢',
+    Variable = '󰆦',
+    Property = '󰖷',
+
+    Class = '󱡠',
+    Interface = '󱡠',
+    Struct = '󱡠',
+    Module = '󰅩',
+
+    Unit = '󰪚',
+    Value = '󰦨',
+    Enum = '󰦨',
+    EnumMember = '󰦨',
+
+    Keyword = '󰻾',
+    Constant = '󰏿',
+
+    Snippet = '󱄽',
+    Color = '󰏘',
+    File = '󰈔',
+    Reference = '󰬲',
+    Folder = '󰉋',
+    Event = '󱐋',
+    Operator = '󰪚',
+    TypeParameter = '󰬛',
+  },
+  keymap = {
+    ['<Tab>'] = 'accept',
+    ['<C-j>'] = 'select_prev',
+    ['<Down>'] = 'select_prev',
+    ['<C-k>'] = 'select_next',
+    ['<Up>'] = 'select_next',
+    ['<C-space>'] = 'toggle',
+  },
+}
+
+function M.setup(opts) M.config = vim.tbl_deep_extend('force', M.default, opts or {}) end
+
+return setmetatable(M, { __index = function(_, k) return M.config[k] end })
