08bb59422b9b18b76038ee4f17ae6e48d1d01029
Gabe Dunn <gabe@sent.at>
2024-07-25 15:53:58 -0600

    chore: remove fuzzy (moved to own repo)

--- lua/blink/cmp/sources/path.lua
+++
@@ -1,8 +0,0 @@
-local path = {}
-
-function path.completions(callback)
-  local bufnr = vim.api.nvim_get_current_buf()
-  local buffer_path = vim.api.nvim_buf_get_name(bufnr)
-
-  local cwd = vim.fn.expand('%:p:h', buffer_path)
-end
