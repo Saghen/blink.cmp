864db38b0bf416b6c394fd0b6fa6a9faf11ded63
Gabe Dunn <gabe@sent.at>
2024-07-25 15:53:58 -0600

    chore: remove fuzzy (moved to own repo)

--- flake.nix
+++ flake.nix
@@ -3,18 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
-    devenv.url = "github:cachix/devenv";
     flake-parts.url = "github:hercules-ci/flake-parts";
-    fenix.url = "github:nix-community/fenix";
-    fenix.inputs.nixpkgs.follows = "nixpkgs";
-    naersk.url = "github:nix-community/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  nixConfig = {
-    extra-trusted-public-keys =
-      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-    extra-substituters = "https://devenv.cachix.org";
   };
 
   outputs = inputs@{ flake-parts, nixpkgs, fenix, naersk, ... }:
@@ -31,28 +20,12 @@
       perSystem = { config, self', inputs', pkgs, system, lib, ... }: {
         # define the packages provided by this flake
         packages = {
-          fuzzy = let
-            # use the minimal nightly toolchain provided by fenix
-            toolchain = inputs'.fenix.packages.minimal.toolchain;
-            nearskLib = inputs.naersk.lib.${system}.override {
-              cargo = toolchain;
-              rustc = toolchain;
-            };
-          in nearskLib.buildPackage { src = ./lua/blink/cmp/fuzzy; };
-        };
-
-        # define the default dev environment
-        devenv.shells.default = {
-          name = "fuzzy";
-
-          languages.rust = {
-            enable = true;
-            channel = "nightly";
+          blink-nvim = pkgs.vimUtils.buildVimPlugin {
+            name = "blink-nvim";
+            src = ./.;
           };
 
-          packages = [ config.packages.fuzzy pkgs.gnumake ];
-
-          scripts.build.exec = "make";
+					default = self'.packages.blink-nvim;
         };
       };
     };
