863bad7d66d616b6498c7d9ba59249138517689a
Liam Dyer <liamcdyer@gmail.com>
2024-10-02 16:46:16 -0400

    fix: keymaps

--- lua/blink/cmp/keymap.lua
+++ lua/blink/cmp/keymap.lua
@@ -15,6 +15,38 @@ local snippet_commands = { 'snippet_forward', 'snippet_backward' }
 
 --- @param opts blink.cmp.KeymapConfig
 function keymap.setup(opts)
+  local function handle_key(mode, key)
+    local key_insert_commands = {}
+    local key_snippet_commands = {}
+
+    for command, keys in pairs(opts) do
+      keys = type(keys) == 'string' and { keys } or keys
+      if vim.tbl_contains(keys, key) then
+        if vim.tbl_contains(snippet_commands, command) then
+          table.insert(key_snippet_commands, command)
+        elseif mode == 'i' and vim.tbl_contains(insert_commands, command) then
+          table.insert(key_insert_commands, command)
+        else
+          error('Invalid command in keymap config: ' .. command)
+        end
+      end
+    end
+
+    for _, command in ipairs(key_insert_commands) do
+      local did_run = require('blink.cmp')[command]()
+      if did_run then return end
+    end
+
+    for _, command in ipairs(key_snippet_commands) do
+      local did_run = require('blink.cmp')[command]()
+      if did_run then return end
+    end
+
+    return key
+  end
+
+  local snippet_keys = {}
+  local insert_keys = {}
   for command, keys in pairs(opts) do
     local is_snippet_command = vim.tbl_contains(snippet_commands, command)
     local is_insert_command = vim.tbl_contains(insert_commands, command)
@@ -25,12 +57,16 @@ function keymap.setup(opts)
 
     -- add keymaps
     for _, key in ipairs(keys) do
-      local mode = is_snippet_command and 's' or 'i'
-      vim.keymap.set(mode, key, function()
-        local did_run = require('blink.cmp')[command]()
-        if not did_run then return key end
-      end, { expr = true, silent = true })
+      insert_keys[key] = true
+      if is_snippet_command then snippet_keys[key] = true end
     end
+  end
+
+  for key, _ in pairs(insert_keys) do
+    vim.keymap.set('i', key, function() return handle_key('i', key) end, { expr = true, silent = true })
+  end
+  for key, _ in pairs(snippet_keys) do
+    vim.keymap.set('s', key, function() return handle_key('s', key) end, { expr = true, silent = true })
   end
 end
 
