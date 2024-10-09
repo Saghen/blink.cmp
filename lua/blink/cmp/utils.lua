88f71b16ecd650775516bd2b30ab808283b7242c
Liam Dyer <liamcdyer@gmail.com>
2024-10-09 19:18:01 -0400

    fix: add union to utils

--- lua/blink/cmp/utils.lua
+++ lua/blink/cmp/utils.lua
@@ -12,6 +12,22 @@ function utils.shallow_copy(t)
   return t2
 end
 
+--- Returns the union of the keys of two tables
+--- @generic T
+--- @param t1 T[]
+--- @param t2 T[]
+--- @return T[]
+function utils.union_keys(t1, t2)
+  local t3 = {}
+  for k, _ in pairs(t1) do
+    t3[k] = true
+  end
+  for k, _ in ipairs(t2) do
+    t3[k] = true
+  end
+  return vim.tbl_keys(t3)
+end
+
 --- Determines whether the current buffer is a "special" buffer
 --- @return boolean
 function utils.is_special_buffer()
