64673ea454f46664ac6f6545f5d3577fd27421e9
Liam Dyer <liamcdyer@gmail.com>
2024-10-09 12:18:02 -0400

    fix: re-enable preselect by default

--- lua/blink/cmp/config.lua
+++ lua/blink/cmp/config.lua
@@ -247,7 +247,7 @@ local config = {
       -- falling back to the next direction when there's not enough space
       direction_priority = { 's', 'n' },
       -- whether to preselect the first item in the completion list
-      preselect = false,
+      preselect = true,
       -- Controls how the completion items are rendered on the popup window
       -- 'simple' will render the item's kind icon the left alongside the label
       -- 'reversed' will render the label on the left and the kind icon + name on the right
