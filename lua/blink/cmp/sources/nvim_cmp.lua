03a5cef5f4b22785a4fe0836bc88f0b291b84264
Stefan Boca <stefan.r.boca@gmail.com>
2024-10-10 13:30:52 -0700

    fix(nvim-cmp): lazily look up source in registry

--- lua/blink/cmp/sources/nvim_cmp.lua
+++ lua/blink/cmp/sources/nvim_cmp.lua
@@ -1,3 +1,5 @@
+local nvim_cmp_registry = require('blink.cmp.sources.lib').nvim_cmp_registry
+
 --- @class blink.cmp.Source
 --- @field name string
 --- @field opts table | nil
@@ -8,13 +10,13 @@ function nvim_cmp.new(opts, name)
   local self = setmetatable(nvim_cmp, { _index = nvim_cmp })
   self.name = name
   self.opts = opts
-  self.source = require('blink.cmp.sources.lib').nvim_cmp_registry:get_source(name)
 
   return self
 end
 
 function nvim_cmp:get_completions(ctx, callback)
-  if self.source.complete == nil then
+  local source = nvim_cmp_registry:get_source(self.name)
+  if source == nil or source.complete == nil then
     --- @diagnostic disable: missing-parameter
     return callback()
   end
@@ -41,7 +43,7 @@ function nvim_cmp:get_completions(ctx, callback)
     },
   }
 
-  if not self.source.complete(self.source, params, transformed_callback) then
+  if not source:complete(params, transformed_callback) then
     --- @diagnostic disable: missing-parameter
     return callback()
   end
@@ -50,14 +52,16 @@ function nvim_cmp:get_completions(ctx, callback)
 end
 
 function nvim_cmp:get_trigger_characters()
+  local source = nvim_cmp_registry:get_source(self.name)
   --- @diagnostic disable: return-type-mismatch
-  if self.source.get_trigger_characters == nil then return nil end
-  return self.source:get_trigger_characters()
+  if source == nil or source.get_trigger_characters == nil then return nil end
+  return source:get_trigger_characters()
 end
 
 function nvim_cmp:should_show_completions()
-  if self.source.is_available == nil then return true end
-  return self.source:is_available()
+  local source = nvim_cmp_registry:get_source(self.name)
+  if source == nil or source.is_available == nil then return true end
+  return source:is_available()
 end
 
 return nvim_cmp
