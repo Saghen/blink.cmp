dd5094aa19733c366f1eff96dd4e4ccaf7fa47d8
Liam Dyer <liamcdyer@gmail.com>
2024-10-10 20:11:02 -0400

    chore: bump frizbee closes #22 Includes a fix for fuzzy matching on entries with >256 characters

--- flake.nix
+++ flake.nix
@@ -48,7 +48,7 @@
                 "c-marshalling-0.2.0" =
                   "sha256-eL6nkZOtuLLQ0r31X7uroUUDYZsWOJ9KNXl4NCVNRuw=";
                 "frizbee-0.1.0" =
-                  "sha256-9L3ZS7GMvLEqOBjC/VW2jHEZge/s6jRN7ok647Frl/M=";
+                  "sha256-bm/wkK/ASLqDbjfRvRtLPKfLIiRV8i3hUWdpb977uto=";
               };
             };
           };
