690f93cb520453ccf7ad9320bf869285c0014d04
Radoje <megasrbin1@gmail.com>
2024-10-12 19:50:41 +0200

    make it a bit cleaner

--- lua/blink/cmp/keymap.lua
+++ lua/blink/cmp/keymap.lua
@@ -14,6 +14,8 @@ local insert_commands = {
 }
 local snippet_commands = { 'snippet_forward', 'snippet_backward' }
 
+local exclude_filetypes = { 'TelescopePrompt' }
+
 --- @param opts blink.cmp.KeymapConfig
 function keymap.setup(opts)
   local insert_keys_to_commands = {}
@@ -51,7 +53,7 @@ end
 --- @param insert_keys_to_commands table<string, string[]>
 --- @param snippet_keys_to_commands table<string, string[]>
 function keymap.apply_keymap_to_current_buffer(insert_keys_to_commands, snippet_keys_to_commands)
-  if vim.bo.ft == 'TelescopePrompt' then return end
+  if exclude_filetypes[vim.bo.ft] then return end
   -- skip if we've already applied the keymaps
   for _, mapping in ipairs(vim.api.nvim_buf_get_keymap(0, 'i')) do
     if mapping.desc == 'blink.cmp' then return end
