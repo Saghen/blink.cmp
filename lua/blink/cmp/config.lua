cf636730d240adf01eee02f54c9083f07d65db45
Scott McKendry <39483124+scottmckendry@users.noreply.github.com>
2024-10-14 07:00:55 +1300

    feat(config): add ignored filetypes option

--- lua/blink/cmp/config.lua
+++ lua/blink/cmp/config.lua
@@ -133,6 +133,7 @@
 --- @field highlight? blink.cmp.HighlightConfig
 --- @field nerd_font_variant? 'mono' | 'normal'
 --- @field kind_icons? table<string, string>
+--- @field blocked_filetypes? string[]
 
 --- @type blink.cmp.Config
 local config = {
@@ -305,6 +306,9 @@ local config = {
   -- set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
   -- adjusts spacing to ensure icons are aligned
   nerd_font_variant = 'normal',
+
+  -- don't show completions or signature help for these filetypes. Keymaps are also disabled.
+  blocked_filetypes = {},
 
   kind_icons = {
     Text = '󰉿',
--- lua/blink/cmp/utils.lua
+++ lua/blink/cmp/utils.lua
@@ -28,11 +28,15 @@ function utils.union_keys(t1, t2)
   return vim.tbl_keys(t3)
 end
 
---- Determines whether the current buffer is a "special" buffer
+--- Determines whether the current buffer is a "special" buffer or if the filetype is in the list of ignored filetypes
 --- @return boolean
-function utils.is_special_buffer()
+function utils.is_ignored_buffer()
   local bufnr = vim.api.nvim_get_current_buf()
   local buftype = vim.api.nvim_get_option_value('buftype', { buf = bufnr })
+  local blocked_filetypes = require('blink.cmp.config').blocked_filetypes or {}
+  local buf_filetype = vim.api.nvim_get_option_value('filetype', { buf = bufnr })
+
+  if vim.tbl_contains(blocked_filetypes, buf_filetype) then return true end
   return buftype ~= ''
 end
 
