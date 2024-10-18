cbda28d0d1045b90176dcf09acab282e41af3b16
Liam Dyer <liamcdyer@gmail.com>
2024-10-18 14:35:06 -0400

    feat: improve multi-platform support

--- lua/blink/cmp/fuzzy/init.lua
+++ lua/blink/cmp/fuzzy/init.lua
@@ -1,23 +1,10 @@
-local function load_rust()
-  local platform = jit.os:lower()
-  if platform == 'osx' or platform == 'mac' then
-    return require('blink.cmp.fuzzy.rust-macos')
-  elseif platform == 'windows' then
-    return require('blink.cmp.fuzzy.rust-win')
-  else
-    return require('blink.cmp.fuzzy.rust-linux')
-  end
-end
-
-----------------
-
 local config = require('blink.cmp.config').fuzzy
 local fuzzy = {
   ---@type blink.cmp.Context?
   last_context = nil,
   ---@type blink.cmp.CompletionItem[]?
   last_items = nil,
-  rust = load_rust(),
+  rust = require('blink.cmp.fuzzy.rust'),
 }
 
 ---@param db_path string
