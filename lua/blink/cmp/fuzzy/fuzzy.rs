63b7b2219f4595973225a13e6e664fc836ee305c
Liam Dyer <liamcdyer@gmail.com>
2024-10-21 18:00:19 -0400

    feat: expose typo resistance, update frizbee

--- lua/blink/cmp/fuzzy/fuzzy.rs
+++ lua/blink/cmp/fuzzy/fuzzy.rs
@@ -58,7 +58,9 @@ pub struct MatchedLspItem {
 
 #[derive(Clone, Serialize, Deserialize, Hash)]
 pub struct FuzzyOptions {
+    use_typo_resistance: bool,
     use_frecency: bool,
+    use_proximity: bool,
     nearby_words: Option<Vec<String>>,
     min_score: u16,
     max_items: u32,
@@ -68,14 +70,18 @@ pub struct FuzzyOptions {
 impl FromLua<'_> for FuzzyOptions {
     fn from_lua(value: LuaValue<'_>, _lua: &'_ Lua) -> LuaResult<Self> {
         if let Some(tab) = value.as_table() {
+            let use_typo_resistance: bool = tab.get("use_typo_resistance").unwrap_or_default();
             let use_frecency: bool = tab.get("use_frecency").unwrap_or_default();
+            let use_proximity: bool = tab.get("use_proximity").unwrap_or_default();
             let nearby_words: Option<Vec<String>> = tab.get("nearby_words").ok();
             let min_score: u16 = tab.get("min_score").unwrap_or_default();
             let max_items: u32 = tab.get("max_items").unwrap_or_default();
             let sorts: Vec<String> = tab.get("sorts").unwrap_or_default();
 
             Ok(FuzzyOptions {
+                use_typo_resistance,
                 use_frecency,
+                use_proximity,
                 nearby_words,
                 min_score,
                 max_items,
@@ -102,9 +108,16 @@ pub fn fuzzy(
     // Fuzzy match with fzrs
     let haystack_labels = haystack
         .iter()
-        .map(|s| s.label.as_str())
+        .map(|s| {
+            if let Some(filter_text) = &s.filter_text {
+                filter_text.as_str()
+            } else {
+                s.label.as_str()
+            }
+        })
         .collect::<Vec<_>>();
     let options = frizbee::Options {
+        prefilter: !opts.use_typo_resistance,
         min_score: opts.min_score,
         stable_sort: false,
         ..Default::default()
@@ -112,7 +125,6 @@ pub fn fuzzy(
     let mut matches = frizbee::match_list(&needle, &haystack_labels, options);
 
     // Sort by scores
-    // TODO: boost exact matches
     let match_scores = matches
         .iter()
         .map(|mtch| {
@@ -121,10 +133,14 @@ pub fn fuzzy(
             } else {
                 0
             };
-            let nearby_words_score = nearby_words
-                .get(&haystack[mtch.index_in_haystack].label)
-                .map(|_| 2)
-                .unwrap_or(0);
+            let nearby_words_score = if opts.use_proximity {
+                nearby_words
+                    .get(&haystack[mtch.index_in_haystack].label)
+                    .map(|_| 2)
+                    .unwrap_or(0)
+            } else {
+                0
+            };
             let score_offset = haystack[mtch.index_in_haystack].score_offset.unwrap_or(0);
 
             (mtch.score as i32) + frecency_score + nearby_words_score + score_offset
@@ -132,12 +148,14 @@ pub fn fuzzy(
         .collect::<Vec<_>>();
 
     // Find the highest score and filter out matches that are unreasonably lower than it
-    let max_score = matches.iter().map(|mtch| mtch.score).max().unwrap_or(0);
-    let secondary_min_score = max_score.max(16) - 16;
-    matches = matches
-        .into_iter()
-        .filter(|mtch| mtch.score >= secondary_min_score)
-        .collect::<Vec<_>>();
+    if opts.use_typo_resistance {
+        let max_score = matches.iter().map(|mtch| mtch.score).max().unwrap_or(0);
+        let secondary_min_score = max_score.max(16) - 16;
+        matches = matches
+            .into_iter()
+            .filter(|mtch| mtch.score >= secondary_min_score)
+            .collect::<Vec<_>>();
+    }
 
     // Sort matches by sort criteria
     for sort in opts.sorts.iter() {
