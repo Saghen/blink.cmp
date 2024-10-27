1a55fd1e03193e10cb8bc866cc2bc47c9473061c
João Mendonça <joaommm0312@gmail.com>
2024-10-27 19:22:57 +0000

    fix: schedule non-expr fallback keymaps (#196)
    * fix(keymaps): conflicts
    
    fix(keymaps): conflicting with other runtime keymaps
    
    * fix: schedule non-expr fallback keymaps
    
    ---------
    
    Co-authored-by: Liam Dyer <liamcdyer@gmail.com>

--- lua/blink/cmp/keymap.lua
+++ lua/blink/cmp/keymap.lua
@@ -117,10 +117,16 @@ function keymap.run_non_blink_keymap(mode, key)
   -- and is quite complex. we should look to see if we can simplify their logic
   -- https://github.com/hrsh7th/nvim-cmp/blob/ae644feb7b67bf1ce4260c231d1d4300b19c6f30/lua/cmp/utils/keymap.lua
   if type(mapping.callback) == 'function' then
+    -- with expr = true, which we use, we can't modify the buffer without scheduling
+    -- so if the keymap does not use expr, we must schedule it
+    if mapping.expr ~= 1 then
+      vim.schedule(mapping.callback)
+      return
+    end
+
     local expr = mapping.callback()
     if mapping.replace_keycodes == 1 then expr = vim.api.nvim_replace_termcodes(expr, true, true, true) end
-    if mapping.expr == 1 then return expr end
-    return
+    return expr
   elseif mapping.rhs then
     local rhs = vim.api.nvim_replace_termcodes(mapping.rhs, true, true, true)
     if mapping.expr == 1 then rhs = vim.api.nvim_eval(rhs) end
