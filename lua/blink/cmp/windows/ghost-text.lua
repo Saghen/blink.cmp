c09a5dbe83cd4fcd3c8bf1e21693c2c5b8eb5323
giuxtaposition <yg97.cs@gmail.com>
2024-11-04 09:53:12 +0100

    fix(ghost-text): flickering using decoration provider and redraw function

--- lua/blink/cmp/windows/ghost-text.lua
+++ lua/blink/cmp/windows/ghost-text.lua
@@ -1,23 +1,23 @@
 local config = require('blink.cmp.config')
 local autocomplete = require('blink.cmp.windows.autocomplete')
+local text_edits_lib = require('blink.cmp.accept.text-edits')
+local snippets_utils = require('blink.cmp.sources.snippets.utils')
 
 local ghost_text_config = config.windows.ghost_text
 
+---@class blink.cmp.windows.ghost_text
+---@field enabled boolean
+---@field ns_id integer
+---@field extmark_id integer?
+---@field win integer?
+---@field selected_item blink.cmp.CompletionItem?
 local ghost_text = {
-  enabled = ghost_text_config and ghost_text_config.enabled,
-  extmark_id = 1,
-  ns_id = config.highlight.ns,
+  enabled = ghost_text_config and ghost_text_config.enabled or false,
+  ns_id = vim.api.nvim_create_namespace('blink_cmp.ghost_text'),
+  extmark_id = nil,
+  win = nil,
+  selected_item = nil,
 }
-
-function ghost_text.setup()
-  autocomplete.listen_on_select(function(item)
-    if ghost_text.enabled ~= true then return end
-    ghost_text.show_preview(item)
-  end)
-  autocomplete.listen_on_close(function() ghost_text.clear_preview() end)
-
-  return ghost_text
-end
 
 --- @param textEdit lsp.TextEdit
 local function get_still_untyped_text(textEdit)
@@ -26,41 +26,83 @@ local function get_still_untyped_text(textEdit)
   return result
 end
 
---- @param selected_item? blink.cmp.CompletionItem
-function ghost_text.show_preview(selected_item)
-  if selected_item == nil then return end
-  local text_edits_lib = require('blink.cmp.accept.text-edits')
-  local text_edit = text_edits_lib.get_from_item(selected_item)
+function ghost_text.setup()
+  local self = setmetatable({}, { __index = ghost_text })
 
-  if selected_item.insertTextFormat == vim.lsp.protocol.InsertTextFormat.Snippet then
-    local expanded_snippet = require('blink.cmp.sources.snippets.utils').safe_parse(text_edit.newText)
-    text_edit.newText = expanded_snippet and tostring(expanded_snippet) or text_edit.newText
-  end
+  vim.api.nvim_set_decoration_provider(self.ns_id, {
+    on_win = function(_, win)
+      if self.extmark_id then
+        vim.api.nvim_buf_del_extmark(vim.api.nvim_win_get_buf(self.win), self.ns_id, self.extmark_id)
+        self.extmark_id = nil
+      end
 
-  local display_lines = vim.split(get_still_untyped_text(text_edit), '\n', { plain = true }) or {}
+      if win ~= self.win then return false end
 
-  --- @type vim.api.keyset.set_extmark
-  local extmark = {
-    id = ghost_text.extmark_id,
-    virt_text_pos = 'inline',
-    virt_text = { { display_lines[1], 'BlinkCmpGhostText' } },
-    hl_mode = 'combine',
-  }
+      if (not self.enabled) or not self.selected_item then return end
 
-  if #display_lines > 1 then
-    extmark.virt_lines = {}
-    for i = 2, #display_lines do
-      extmark.virt_lines[i - 1] = { { display_lines[i], 'BlinkCmpGhostText' } }
-    end
-  end
+      local text_edit = text_edits_lib.get_from_item(self.selected_item)
 
-  local cursor_pos = {
-    text_edit.range.start.line,
-    text_edit.range['end'].character,
-  }
-  vim.api.nvim_buf_set_extmark(0, ghost_text.ns_id, cursor_pos[1], cursor_pos[2], extmark)
+      if self.selected_item.insertTextFormat == vim.lsp.protocol.InsertTextFormat.Snippet then
+        local expanded_snippet = snippets_utils.safe_parse(text_edit.newText)
+        text_edit.newText = expanded_snippet and tostring(expanded_snippet) or text_edit.newText
+      end
+
+      local display_lines = vim.split(get_still_untyped_text(text_edit), '\n', { plain = true }) or {}
+
+      local virt_lines = {}
+      if #display_lines > 1 then
+        for i = 2, #display_lines do
+          virt_lines[i - 1] = { { display_lines[i], 'BlinkCmpGhostText' } }
+        end
+      end
+
+      local cursor_pos = {
+        text_edit.range.start.line,
+        text_edit.range['end'].character,
+      }
+
+      self.extmark_id = vim.api.nvim_buf_set_extmark(
+        vim.api.nvim_win_get_buf(self.win),
+        ghost_text.ns_id,
+        cursor_pos[1],
+        cursor_pos[2],
+        {
+          virt_text_pos = 'inline',
+          virt_text = { { display_lines[1], 'BlinkCmpGhostText' } },
+          virt_lines = virt_lines,
+          hl_mode = 'combine',
+          ephemeral = false,
+        }
+      )
+    end,
+  })
+
+  autocomplete.listen_on_select(function(item)
+    if ghost_text.enabled ~= true then return end
+    ghost_text:show_preview(item)
+  end)
+  autocomplete.listen_on_close(function() ghost_text:clear_preview() end)
+
+  return ghost_text
 end
 
-function ghost_text.clear_preview() vim.api.nvim_buf_del_extmark(0, ghost_text.ns_id, ghost_text.extmark_id) end
+--- @param selected_item? blink.cmp.CompletionItem
+function ghost_text:show_preview(selected_item)
+  if not selected_item then
+    self.selected_item = nil
+    return
+  end
+  local changed = self.selected_item ~= selected_item
+  self.selected_item = selected_item
+  self.win = vim.api.nvim_get_current_win()
+  if changed then vim.cmd.redraw({ bang = true }) end
+end
+
+function ghost_text:clear_preview()
+  if self.win and self.selected_item then
+    self.selected_item = nil
+    vim.cmd.redraw({ bang = true })
+  end
+end
 
 return ghost_text
