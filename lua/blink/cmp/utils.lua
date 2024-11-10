140ae95b4ce7f9c1484eadc2ef38fdb7c748980f
lopy <70210066+lopi-py@users.noreply.github.com>
2024-11-09 22:42:45 -0500

    fix: tailwind colors

--- lua/blink/cmp/utils.lua
+++ lua/blink/cmp/utils.lua
@@ -85,15 +85,13 @@ end
 
 --- @param ctx blink.cmp.CompletionRenderContext
 --- @return string|nil
-function utils.try_get_tailwind_hl(ctx)
+function utils.get_tailwind_hl(ctx)
   local doc = ctx.item.documentation
   if ctx.kind == 'Color' and doc then
     local content = type(doc) == 'string' and doc or doc.value
-    if ctx.kind == 'Color' and content and content:match('^#%x%x%x%x%x%x$') then
+    if content and content:match('^#%x%x%x%x%x%x$') then
       local hl_name = 'HexColor' .. content:sub(2)
-      if #vim.api.nvim_get_hl(0, { name = hl_name }) == 0 then
-        vim.api.nvim_set_hl(0, hl_name, { fg = content })
-      end
+      if #vim.api.nvim_get_hl(0, { name = hl_name }) == 0 then vim.api.nvim_set_hl(0, hl_name, { fg = content }) end
       return hl_name
     end
   end
