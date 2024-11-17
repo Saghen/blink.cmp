cba25a4cd444e5ef00c094611de6b80ee12e05e4
John Drouhard <john@drouhard.dev>
2024-11-17 09:25:16 -0600

    feat: extract word from completion item for auto-insert preview
    Use a modified version of nvim-cmp's word extraction algorithm from
    expanded snippets for use in auto-insert preview textEdits.
    
    For example, if a snippet expansion results in something like:
    
    ```
    my_neat_function(int arg1, double arg2)
    ```
    
    Then the floating window will show the above for that completion, but
    when selecting it, the buffer will only insert `my_neat_function`. This
    allows using the snippet completion item without actually _accepting_ it
    by just selecting it in the preview window and then continuing to type.
    
    So one could type "my_n", Ctrl-N/Tab (or whatever your keymap is) to
    select `my_neat_function`, and then just continue with typing the ( and
    args manually. Of course, accepting the completion will still work as
    expected by expanding the snippet.

--- lua/blink/cmp/accept/preview.lua
+++ lua/blink/cmp/accept/preview.lua
@@ -1,3 +1,56 @@
+local _
+local INVALID_CHARS = {}
+_ = string.gsub('\'"=$()[]<>{} \t\n\r', '.', function(char)
+  INVALID_CHARS[string.byte(char)] = true
+end)
+
+local PAIRS = {}
+PAIRS[string.byte('<')] = string.byte('>')
+PAIRS[string.byte('[')] = string.byte(']')
+PAIRS[string.byte('(')] = string.byte(')')
+PAIRS[string.byte('{')] = string.byte('}')
+PAIRS[string.byte('"')] = string.byte('"')
+PAIRS[string.byte("'")] = string.byte("'")
+
+local ALPHANUMERIC = {}
+_ = string.gsub('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', '.', function(char)
+  ALPHANUMERIC[string.byte(char)] = true
+end)
+
+---get_word
+---@param text string
+---@return string
+local get_word = function(text)
+  local has_alnum = false
+  local stack = {}
+  local word = {}
+  local add = function(c)
+    table.insert(word, string.char(c))
+    if stack[#stack] == c then
+      table.remove(stack, #stack)
+    else
+      if PAIRS[c] then
+        table.insert(stack, PAIRS[c])
+      end
+    end
+  end
+  for i = 1, #text do
+    local c = string.byte(text, i)
+    if not INVALID_CHARS[c] then
+      add(c)
+      has_alnum = has_alnum or ALPHANUMERIC[c]
+    elseif not has_alnum or #stack ~= 0 then
+      add(c)
+      if has_alnum and #stack == 0 then
+        break
+      end
+    else
+      break
+    end
+  end
+  return table.concat(word, '')
+end
+
 --- @param item blink.cmp.CompletionItem
 local function preview(item, previous_text_edit)
   local text_edits_lib = require('blink.cmp.accept.text-edits')
@@ -6,22 +59,15 @@ local function preview(item, previous_text_edit)
   -- with auto_insert, we may have to undo the previous preview
   if previous_text_edit ~= nil then text_edit.range = text_edits_lib.get_undo_range(previous_text_edit) end
 
-  -- for snippets, expand them with the default property names
+  if item.insertTextFormat == vim.lsp.protocol.InsertTextFormat.Snippet then
+    local expanded_snippet = require('blink.cmp.sources.snippets.utils').safe_parse(text_edit.newText)
+    text_edit.newText = get_word(expanded_snippet and tostring(expanded_snippet) or text_edit.newText)
+  end
+
   local cursor_pos = {
     text_edit.range.start.line + 1,
     text_edit.range.start.character + #text_edit.newText,
   }
-  if item.insertTextFormat == vim.lsp.protocol.InsertTextFormat.Snippet then
-    local expanded_snippet = require('blink.cmp.sources.snippets.utils').safe_parse(text_edit.newText)
-    text_edit.newText = expanded_snippet and tostring(expanded_snippet) or text_edit.newText
-
-    -- place the cursor at the first tab stop
-    local tabstops = require('blink.cmp.sources.snippets.utils').get_tab_stops(text_edit.newText)
-    if tabstops and #tabstops > 0 then
-      cursor_pos[1] = text_edit.range.start.line + tabstops[1].line
-      cursor_pos[2] = text_edit.range.start.character + tabstops[1].character
-    end
-  end
 
   text_edits_lib.apply({ text_edit })
   vim.api.nvim_win_set_cursor(0, cursor_pos)
