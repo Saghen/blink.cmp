a07f07927a767f4e1dd5e7e8b928e3955070d3f9
Folke Lemaitre <folke.lemaitre@gmail.com>
2024-11-29 19:15:40 +0100

    perf(treesitter): cache treesitter highlights

--- lua/blink/cmp/completion/windows/render/treesitter.lua
+++ lua/blink/cmp/completion/windows/render/treesitter.lua
@@ -1,13 +1,44 @@
 local treesitter = {}
 
+---@type table<string, blink.cmp.DrawHighlight[]>
+local cache = {}
+local cache_size = 0
+local MAX_CACHE_SIZE = 1000
+
 --- @param ctx blink.cmp.DrawItemContext
 --- @param opts? {offset?: number}
 function treesitter.highlight(ctx, opts)
-  ---@type blink.cmp.DrawHighlight[]
-  local ret = {}
-  local source = ctx.label
-  local offset = opts and opts.offset or 0
+  -- early return if treesitter highlight is disabled
+  if not vim.b.ts_highlight then return {} end
 
+  local ret = cache[ctx.label]
+  if not ret then
+    -- cleanup cache if it's too big
+    cache_size = cache_size + 1
+    if cache_size > MAX_CACHE_SIZE then
+      cache = {}
+      cache_size = 0
+    end
+    ret = treesitter._highlight(ctx)
+    cache[ctx.label] = ret
+  end
+
+  -- offset highlights if needed
+  if opts and opts.offset then
+    ret = vim.deepcopy(ret)
+    for _, hl in ipairs(ret) do
+      hl[1] = hl[1] + opts.offset
+      hl[2] = hl[2] + opts.offset
+    end
+  end
+  return ret
+end
+
+--- @param ctx blink.cmp.DrawItemContext
+function treesitter._highlight(ctx)
+  local ret = {} ---@type blink.cmp.DrawHighlight[]
+
+  local source = ctx.label
   local lang = vim.treesitter.language.get_lang(vim.bo.filetype)
   if not lang then return ret end
 
@@ -29,8 +60,8 @@ function treesitter.highlight(ctx, opts)
       local name = query.captures[capture]
       if name ~= 'spell' then
         ret[#ret + 1] = {
-          offset + start_col,
-          offset + end_col,
+          start_col,
+          end_col,
           group = '@' .. name .. '.' .. lang,
         }
       end
