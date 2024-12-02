3e94cd1784855d2e365017b86dc31ebfeb48d837
Liam Dyer <liamcdyer@gmail.com>
2024-12-02 12:37:58 -0500

    style: get_buf_text return type

--- lua/blink/cmp/sources/buffer.lua
+++ lua/blink/cmp/sources/buffer.lua
@@ -5,7 +5,7 @@
 local uv = vim.uv
 
 --- @param bufnr integer
----@return string
+--- @return string
 local function get_buf_text(bufnr)
   local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
