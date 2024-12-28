f3c8e38991d1ab4effa69a4fd1a7b4549f7f32cf
bydlw98 <108581910+bydlw98@users.noreply.github.com>
2024-12-28 22:25:31 +0800

    feat: add `BlinkCmpStatus` command
    The command `:BlinkCmpStatus` can be used to view which sources
    providers are enabled or not enabled.
    
    This is inspired by and implemented based on the `:CmpStatus` command
    from `hrsh7th/nvim-cmp`.

--- lua/blink/cmp/init.lua
+++ lua/blink/cmp/init.lua
@@ -184,4 +184,42 @@ function cmp.add_provider(id, provider_config)
   config.sources.providers[id] = provider_config
 end
 
+function cmp.status()
+  local sources = require('blink.cmp.sources.lib')
+  local all_providers = sources.get_all_providers()
+  local enabled_provider_ids = sources.get_enabled_provider_ids('default')
+
+  --- @type string[]
+  local not_enabled_provider_ids = {}
+  for provider_id, _ in pairs(all_providers) do
+    if not vim.list_contains(enabled_provider_ids, provider_id) then
+      table.insert(not_enabled_provider_ids, provider_id)
+    end
+  end
+
+  if #enabled_provider_ids > 0 then
+    vim.api.nvim_echo({ { '\n', 'Normal' } }, false, {})
+    vim.api.nvim_echo({ { '# enabled sources providers\n', 'Special' } }, false, {})
+
+    for _, provider_id in ipairs(enabled_provider_ids) do
+      vim.api.nvim_echo({ { ('- %s\n'):format(provider_id), 'Normal' } }, false, {})
+    end
+  end
+
+  if #not_enabled_provider_ids > 0 then
+    vim.api.nvim_echo({ { '\n', 'Normal' } }, false, {})
+    vim.api.nvim_echo({ { '# not enabled sources providers\n', 'Comment' } }, false, {})
+
+    for _, provider_id in pairs(not_enabled_provider_ids) do
+      vim.api.nvim_echo({ { ('- %s\n'):format(provider_id), 'Normal' } }, false, {})
+    end
+  end
+end
+
+vim.api.nvim_create_user_command(
+  'BlinkCmpStatus',
+  function() cmp.status() end,
+  { desc = 'Check status of blink.cmp sources providers' }
+)
+
 return cmp
