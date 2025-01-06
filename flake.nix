07c261e95022f645823f9443e21564f0208f2f2b
Konrad Malik <konrad.malik@gmail.com>
2025-01-06 12:28:44 +0100

    ci(nix): check, build, run, even on prs
    Also adds nix-comminity cache to avoid builds from fenix.

--- flake.nix
+++ flake.nix
@@ -106,4 +106,11 @@
         formatter = pkgs.nixfmt-classic;
       };
     };
+
+  nixConfig = {
+    extra-substituters = [ "https://nix-community.cachix.org" ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs"
+    ];
+  };
 }
