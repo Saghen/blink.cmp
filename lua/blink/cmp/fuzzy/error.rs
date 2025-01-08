3fa7da1cd7876d42d4e7ac5f3414aac2bf1644b6
Stefan Boca <stefan.r.boca@gmail.com>
2025-01-07 23:43:15 -0800

    refactor(fuzzy): add error type using thiserror crate
    IMHO extracting error messages makes the code more readable
    This also decouples everything but lib.rs and the lua
    type wrappers from mlua

---
+++ lua/blink/cmp/fuzzy/error.rs
@@ -0,0 +1,44 @@
+#[derive(thiserror::Error, Debug)]
+#[non_exhaustive]
+pub enum Error {
+    #[error("Failed to acquire lock for frecency")]
+    AcquireFrecencyLock,
+
+    #[error("Failed to acquire lock for items by provider")]
+    AcquireItemLock,
+
+    #[error("Attempted to use frencecy before initialization")]
+    UseFrecencyBeforeInit,
+
+    #[error(
+        "Attempted to fuzzy match for provider {provider_id} before setting the provider's items"
+    )]
+    FuzzyBeforeSetItems { provider_id: String },
+
+    #[error("Failed to create frecency database directory: {0}")]
+    CreateDir(#[source] std::io::Error),
+    #[error("Failed to open frecency database env: {0}")]
+    EnvOpen(#[source] heed::Error),
+    #[error("Failed to create frecency database: {0}")]
+    DbCreate(#[source] heed::Error),
+    #[error("Failed to clear stale readers for frecency database: {0}")]
+    DbClearStaleReaders(#[source] heed::Error),
+
+    #[error("Failed to start read transaction for frecency database: {0}")]
+    DbStartReadTxn(#[source] heed::Error),
+    #[error("Failed to start write transaction for frecency database: {0}")]
+    DbStartWriteTxn(#[source] heed::Error),
+
+    #[error("Failed to read from frecency database: {0}")]
+    DbRead(#[source] heed::Error),
+    #[error("Failed to write to frecency database: {0}")]
+    DbWrite(#[source] heed::Error),
+    #[error("Failed to commit write transaction to frecency database: {0}")]
+    DbCommit(#[source] heed::Error),
+}
+
+impl From<Error> for mlua::Error {
+    fn from(value: Error) -> Self {
+        mlua::Error::RuntimeError(value.to_string())
+    }
+}
--- lua/blink/cmp/fuzzy/lib.rs
+++ lua/blink/cmp/fuzzy/lib.rs
@@ -1,3 +1,4 @@
+use crate::error::Error;
 use crate::frecency::FrecencyTracker;
 use crate::fuzzy::FuzzyOptions;
 use crate::lsp_item::LspItem;
@@ -6,6 +7,7 @@ use regex::Regex;
 use std::collections::{HashMap, HashSet};
 use std::sync::{LazyLock, RwLock};
 
+mod error;
 mod frecency;
 mod fuzzy;
 mod keyword;
@@ -17,9 +19,7 @@ static HAYSTACKS_BY_PROVIDER: LazyLock<RwLock<HashMap<String, Vec<LspItem>>>> =
     LazyLock::new(|| RwLock::new(HashMap::new()));
 
 pub fn init_db(_: &Lua, (db_path, use_unsafe_no_lock): (String, bool)) -> LuaResult<bool> {
-    let mut frecency = FRECENCY.write().map_err(|_| {
-        mlua::Error::RuntimeError("Failed to acquire lock for frecency".to_string())
-    })?;
+    let mut frecency = FRECENCY.write().map_err(|_| Error::AcquireFrecencyLock)?;
     if frecency.is_some() {
         return Ok(false);
     }
@@ -28,26 +28,18 @@ pub fn init_db(_: &Lua, (db_path, use_unsafe_no_lock): (String, bool)) -> LuaRes
 }
 
 pub fn destroy_db(_: &Lua, _: ()) -> LuaResult<bool> {
-    let frecency = FRECENCY.write().map_err(|_| {
-        mlua::Error::RuntimeError("Failed to acquire lock for frecency".to_string())
-    })?;
+    let frecency = FRECENCY.write().map_err(|_| Error::AcquireFrecencyLock)?;
     drop(frecency);
 
-    let mut frecency = FRECENCY.write().map_err(|_| {
-        mlua::Error::RuntimeError("Failed to acquire lock for frecency".to_string())
-    })?;
+    let mut frecency = FRECENCY.write().map_err(|_| Error::AcquireFrecencyLock)?;
     *frecency = None;
 
     Ok(true)
 }
 
 pub fn access(_: &Lua, item: LspItem) -> LuaResult<bool> {
-    let mut frecency_handle = FRECENCY.write().map_err(|_| {
-        mlua::Error::RuntimeError("Failed to acquire lock for frecency".to_string())
-    })?;
-    let frecency = frecency_handle.as_mut().ok_or_else(|| {
-        mlua::Error::RuntimeError("Attempted to use frencecy before initialization".to_string())
-    })?;
+    let mut frecency = FRECENCY.write().map_err(|_| Error::AcquireFrecencyLock)?;
+    let frecency = frecency.as_mut().ok_or(Error::UseFrecencyBeforeInit)?;
     frecency.access(&item)?;
     Ok(true)
 }
@@ -56,10 +48,10 @@ pub fn set_provider_items(
     _: &Lua,
     (provider_id, items): (String, Vec<LspItem>),
 ) -> LuaResult<bool> {
-    let mut items_by_provider = HAYSTACKS_BY_PROVIDER.write().map_err(|_| {
-        mlua::Error::RuntimeError("Failed to acquire lock for items by provider".to_string())
-    })?;
-    items_by_provider.insert(provider_id, items);
+    HAYSTACKS_BY_PROVIDER
+        .write()
+        .map_err(|_| Error::AcquireItemLock)?
+        .insert(provider_id, items);
     Ok(true)
 }
 
@@ -67,22 +59,15 @@ pub fn fuzzy(
     _lua: &Lua,
     (line, cursor_col, provider_id, opts): (mlua::String, usize, String, FuzzyOptions),
 ) -> LuaResult<(Vec<i32>, Vec<u32>)> {
-    let frecency_handle = FRECENCY.read().map_err(|_| {
-        mlua::Error::RuntimeError("Failed to acquire lock for frecency".to_string())
-    })?;
-    let frecency = frecency_handle.as_ref().ok_or_else(|| {
-        mlua::Error::RuntimeError("Attempted to use frencecy before initialization".to_string())
-    })?;
+    let frecency = FRECENCY.read().map_err(|_| Error::AcquireFrecencyLock)?;
+    let frecency = frecency.as_ref().ok_or(Error::UseFrecencyBeforeInit)?;
 
-    let haystacks_by_provider = HAYSTACKS_BY_PROVIDER.read().map_err(|_| {
-        mlua::Error::RuntimeError("Failed to acquire lock for items by provider".to_string())
-    })?;
-    let haystack = haystacks_by_provider.get(&provider_id).ok_or_else(|| {
-        mlua::Error::RuntimeError(format!(
-            "Attempted to fuzzy match for provider {} before setting the provider's items",
-            provider_id
-        ))
-    })?;
+    let haystacks_by_provider = HAYSTACKS_BY_PROVIDER
+        .read()
+        .map_err(|_| Error::AcquireItemLock)?;
+    let haystack = haystacks_by_provider
+        .get(&provider_id)
+        .ok_or(Error::FuzzyBeforeSetItems { provider_id })?;
 
     Ok(fuzzy::fuzzy(
         &line.to_string_lossy(),
