Saghen/blink.cmp

commit 93541e4e45ddd06cd7efa9d65840936dff557fb3

fix: prefix slicing using chars instead of bytes
Closes #936

lua/blink/cmp/fuzzy/keyword.rs
@@ -52,8 +52,8 @@ pub fn guess_keyword_range_from_item(
     let line_range = get_keyword_range(line, cursor_col, match_suffix);
     let text_range = get_keyword_range(item_text, item_text.len(), false);
 
-    let line_prefix = line.chars().take(line_range.0).collect::<String>();
-    let text_prefix = item_text.chars().take(text_range.0).collect::<String>();
+    let line_prefix = &line[..text_range.0];
+    let text_prefix = &item_text[..text_range.0];
     if line_prefix.ends_with(&text_prefix) {
         return (line_range.0 - text_prefix.len(), line_range.1);
     }

commit 2722bcbc8842b58edcd3d455806f8e059e0b41b1

fix(fuzzy): typo introduced in commit `93541e4`

lua/blink/cmp/fuzzy/keyword.rs
@@ -52,7 +52,7 @@ pub fn guess_keyword_range_from_item(
     let line_range = get_keyword_range(line, cursor_col, match_suffix);
     let text_range = get_keyword_range(item_text, item_text.len(), false);
 
-    let line_prefix = &line[..text_range.0];
+    let line_prefix = &line[..line_range.0];
     let text_prefix = &item_text[..text_range.0];
     if line_prefix.ends_with(&text_prefix) {
         return (line_range.0 - text_prefix.len(), line_range.1);