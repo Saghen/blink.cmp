0abe11770a7b9a23f577a4bd2e3994634f355016
Liam Dyer <liamcdyer@gmail.com>
2025-02-01 15:29:55 -0500

    feat: disable path source when current path segment contains space
    Closes #1126

--- lua/blink/cmp/sources/path/regex.lua
+++ lua/blink/cmp/sources/path/regex.lua
@@ -1,6 +1,17 @@
-local NAME_REGEX = '\\%([^/\\\\:\\*?<>\'"`\\|]\\)'
-local PATH_REGEX =
-  assert(vim.regex(([[\%(\%(/PAT*[^/\\\\:\\*?<>\'"`\\| .~]\)\|\%(/\.\.\)\)*/\zePAT*$]]):gsub('PAT', NAME_REGEX)))
+-- somehow less readable than normal regexes
+
+-- we allow spaces in previous path segments, but not in the current one
+-- Allowed: ~/example foo/
+-- Not allowed: ~/example foo
+local NAME_WITH_SPACE_REGEX = '\\%([^/\\\\:\\*?<>\'"`\\|]\\)'
+local NAME_REGEX = '\\%([^/\\\\:\\*?<>\'"`\\| ]\\)'
+local PATH_REGEX = assert(
+  vim.regex(
+    ([[\%(\%(/PAT1*[^/\\\\:\\*?<>\'"`\\| .~]\)\|\%(/\.\.\)\)*/\zePAT2*$]])
+      :gsub('PAT1', NAME_WITH_SPACE_REGEX)
+      :gsub('PAT2', NAME_REGEX)
+  )
+)
 
 return {
   --- Lua pattern for matching file names
