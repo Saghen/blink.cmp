e66e50e99e735ebee0434567c091068f131ef853
Liam Dyer <liamcdyer@gmail.com>
2025-03-01 14:05:48 -0500

    fix: drop `client_name` from LSP item when resolving
    Related to #1347

--- lua/blink/cmp/sources/lib/utils.lua
+++ lua/blink/cmp/sources/lib/utils.lua
@@ -9,6 +9,7 @@ function utils.blink_item_to_lsp_item(item)
   lsp_item.source_name = nil
   lsp_item.cursor_column = nil
   lsp_item.client_id = nil
+  lsp_item.client_name = nil
   return lsp_item
 end
 
