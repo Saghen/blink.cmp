5821566ec7bab52af15852004742838befc50527
ColdSpirit <coldspiritvolf@gmail.com>
2025-03-13 20:55:41 +0400

    Preserve 'buflisted' state when applying LSP text edits

--- lua/blink/cmp/lib/text_edits.lua
+++ lua/blink/cmp/lib/text_edits.lua
@@ -20,7 +20,12 @@ function text_edits.apply(text_edit, additional_text_edits)
 
     local all_edits = utils.shallow_copy(additional_text_edits)
     table.insert(all_edits, text_edit)
-    vim.lsp.util.apply_text_edits(all_edits, vim.api.nvim_get_current_buf(), 'utf-8')
+
+    -- preserve 'buflisted' state because vim.lsp.util.apply_text_edits forces it to true.  
+    local cur_bufnr = vim.api.nvim_get_current_buf()
+    local prev_buflisted = vim.bo[cur_bufnr].buflisted
+    vim.lsp.util.apply_text_edits(all_edits, cur_bufnr, 'utf-8')
+    vim.bo[cur_bufnr].buflisted = prev_buflisted
   end
 
   if mode == 'cmdline' then
