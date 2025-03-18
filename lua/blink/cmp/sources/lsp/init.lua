1b3fbb5b61b2ec0481a74ce7b149faae3373f370
Liam Dyer <liamcdyer@gmail.com>
2025-03-18 09:24:30 -0400

    refactor: rename `source` to `ctx` in lsp source

--- lua/blink/cmp/sources/lsp/init.lua
+++ lua/blink/cmp/sources/lsp/init.lua
@@ -177,16 +177,16 @@ end
 
 --- Execute ---
 
-function lsp:execute(source, item, callback, default_implementation)
+function lsp:execute(ctx, item, callback, default_implementation)
   default_implementation()
 
   local client = vim.lsp.get_client_by_id(item.client_id)
   if client and item.command then
     if vim.fn.has('nvim-0.11') == 1 then
-      client:exec_cmd(item.command, { bufnr = source.bufnr }, function() callback() end)
+      client:exec_cmd(item.command, { bufnr = ctx.bufnr }, function() callback() end)
     else
       -- TODO: remove this once 0.11 is the minimum version
-      client:_exec_cmd(item.command, { bufnr = source.bufnr }, function() callback() end)
+      client:_exec_cmd(item.command, { bufnr = ctx.bufnr }, function() callback() end)
     end
   else
     callback()
