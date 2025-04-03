61178aa4d11bb1d1b74e30921511b025811b983b
Liam Dyer <liamcdyer@gmail.com>
2025-04-03 11:23:57 -0400

    fix: maintain menu when cursor moves onto trigger character
    Related to #1559

--- lua/blink/cmp/completion/trigger/init.lua
+++ lua/blink/cmp/completion/trigger/init.lua
@@ -91,13 +91,14 @@ local function on_cursor_moved(event, is_ignored)
 
   -- TODO: doesn't handle `a` where the cursor moves immediately after
   -- Reproducible with `example.|a` and pressing `a`, should not show the menu
+  local is_trigger_character = trigger.is_trigger_character(char_under_cursor, true)
   local insert_enter_on_trigger_character = config.show_on_trigger_character
     and config.show_on_insert_on_trigger_character
     and is_enter_event
-    and trigger.is_trigger_character(char_under_cursor, true)
+    and is_trigger_character
 
   -- check if we're still within the bounds of the query used for the context
-  if trigger.context ~= nil and trigger.context:within_query_bounds(cursor) then
+  if trigger.context ~= nil and trigger.context:within_query_bounds(cursor, is_trigger_character) then
     trigger.show({ trigger_kind = 'keyword' })
 
   -- check if we've entered insert mode on a trigger character
@@ -245,7 +246,7 @@ function trigger.show(opts)
 
   local initial_trigger_character = trigger.context and trigger.context.trigger.initial_character
     or opts.trigger_character
-  -- reset the initial character if the context iid has changed
+  -- reset the initial character if the context id has changed
   if trigger.context ~= nil and trigger.context.id ~= trigger.current_context_id then
     initial_trigger_character = nil
   end
