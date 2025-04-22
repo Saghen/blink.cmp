bdb1497c8bb4c331d27e7072420df909c8907f38
Liam Dyer <liamcdyer@gmail.com>
2025-04-22 11:56:20 -0400

    feat: prioritize lowercase over uppercase in `label` sort
    Related to #1642

--- lua/blink/cmp/fuzzy/sort.lua
+++ lua/blink/cmp/fuzzy/sort.lua
@@ -36,7 +36,23 @@ function sort.sort_text(a, b)
   return a.sortText < b.sortText
 end
 
+--- Swaps the case of a single character at index i in string s
+--- @param s string
+--- @param i integer
+--- @return integer
+local function swap_case(s, i)
+  local byte = string.byte(s, i)
+  if byte >= 65 and byte <= 90 then -- uppercase A-Z
+    return byte + 32 -- convert to lowercase
+  elseif byte >= 97 and byte <= 122 then -- lowercase a-z
+    return byte - 32 -- convert to uppercase
+  else
+    return byte -- non-alphabetic characters
+  end
+end
+
 function sort.label(a, b)
+  -- prefer foo_bar over _foo_bar
   local _, entry1_under = a.label:find('^_+')
   local _, entry2_under = b.label:find('^_+')
   entry1_under = entry1_under or 0
@@ -46,7 +62,17 @@ function sort.label(a, b)
   elseif entry1_under < entry2_under then
     return true
   end
-  return a.label < b.label
+
+  -- prefer "a" over "A" and "a" over "b"
+  -- Compare characters one by one with case flipping
+  for i = 1, math.min(#a.label, #b.label) do
+    local char_a = swap_case(a.label, i)
+    local char_b = swap_case(b.label, i)
+
+    if char_a ~= char_b then return char_a < char_b end
+  end
+
+  if #a.label ~= #b.label then return #a.label < #b.label end
 end
 
 return sort
