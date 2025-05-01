ca7e138495b51b6556bb3fd41346341134d26962
Liam Dyer <liamcdyer@gmail.com>
2025-05-01 15:18:25 -0400

    feat: simplify repro.lua

--- repro.lua
+++ repro.lua
@@ -1,4 +1,5 @@
 -- Run with `nvim -u repro.lua`
+-- Please update the code below to reproduce your issue
 
 vim.env.LAZY_STDPATH = '.repro'
 load(vim.fn.system('curl -s https://raw.githubusercontent.com/folke/lazy.nvim/main/bootstrap.lua'))()
@@ -15,19 +16,10 @@ require('lazy.minit').repro({
     },
     {
       'neovim/nvim-lspconfig',
-      opts = {
-        servers = {
-          lua_ls = {},
-        },
-      },
-      config = function(_, opts)
-        local lspconfig = require('lspconfig')
-        for server, config in pairs(opts.servers) do
-          -- passing config.capabilities to blink.cmp merges with the capabilities in your
-          -- `opts[server].capabilities, if you've defined it
-          config.capabilities = require('blink.cmp').get_lsp_capabilities()
-          lspconfig[server].setup(config)
-        end
+      config = function()
+        require('lspconfig').lua_ls.setup({
+          capabilities = require('blink.cmp').get_lsp_capabilities(),
+        })
       end,
     },
   },
