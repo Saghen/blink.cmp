Saghen/blink.cmp

commit 29c8d7267dd73f9b88ae50b169edacf98911f0f6

fix: snippet edit range with lua implementation
Closes #1340

lua/blink/cmp/fuzzy/lua/init.lua
@@ -67,7 +67,24 @@ end
 function fuzzy.get_keyword_range(line, col, match_suffix) return get_keyword_range(line, col, match_suffix) end
 
 function fuzzy.guess_edit_range(item, line, col, match_suffix)
-  return guess_keyword_range_from_item(item.insertText or item.label, line, col, match_suffix)
+  local keyword_start, keyword_end = get_keyword_range(line, col, match_suffix)
+
+  -- Prefer the insert text, then filter text, then label ranges for non-snippets
+  if item.kind ~= require('blink.cmp.types').CompletionItemKind.Snippet then
+    return guess_keyword_range(keyword_start, keyword_end, item.insertText or item.filterText or item.label, line)
+  end
+
+  -- Take the max range prioritizing the start index first and the end index second
+  local label_range = guess_keyword_range(keyword_start, keyword_end, item.label, line)
+  local filter_text_range = item.filterText and guess_keyword_range(keyword_start, keyword_end, item.filterText, line)
+  local insert_text_range = item.insertText and guess_keyword_range(keyword_start, keyword_end, item.insertText, line)
+
+  local ranges = { label_range, filter_text_range, insert_text_range }
+  table.sort(ranges, function(a, b)
+    if a[1] ~= b[1] then return a[1] < b[1] end
+    return a[2] > b[2]
+  end)
+  return ranges[1][1], ranges[1][2]
 end
 
 return fuzzy

commit 03abaa0017b8452615721706b71335a1c458886b

fix: put output values of guess_keyword_range into a table
guess_keyword_range returns multiple values, but only the first values
is used, while the code below expects pair of values.

Fixes: 29c8d7267dd7 ("fix: snippet edit range with lua implementation")

lua/blink/cmp/fuzzy/lua/init.lua
@@ -75,9 +75,13 @@ function fuzzy.guess_edit_range(item, line, col, match_suffix)
   end
 
   -- Take the max range prioritizing the start index first and the end index second
-  local label_range = guess_keyword_range(keyword_start, keyword_end, item.label, line)
-  local filter_text_range = item.filterText and guess_keyword_range(keyword_start, keyword_end, item.filterText, line)
-  local insert_text_range = item.insertText and guess_keyword_range(keyword_start, keyword_end, item.insertText, line)
+  local label_range = { guess_keyword_range(keyword_start, keyword_end, item.label, line) }
+  local filter_text_range = item.filterText
+      and { guess_keyword_range(keyword_start, keyword_end, item.filterText, line) }
+    or label_range
+  local insert_text_range = item.insertText
+      and { guess_keyword_range(keyword_start, keyword_end, item.insertText, line) }
+    or label_range
 
   local ranges = { label_range, filter_text_range, insert_text_range }
   table.sort(ranges, function(a, b)