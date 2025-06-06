b45ce7086c9dc1d6570830dae61b2f0fb8a35566
Soifou <fleur.fr@gmail.com>
2025-06-06 03:54:27 +0200

    refactor(cmdline): improve path completion and argument splitting
    Closes #1600
    Closes #1605
    Closes #1726
    Closes #1858

--- lua/blink/cmp/sources/cmdline/constants.lua
+++ lua/blink/cmp/sources/cmdline/constants.lua
@@ -38,5 +38,9 @@ return {
     diffp = true,
     make = true,
   },
+  completion_types = {
+    buffer = { 'buffer', 'diff_buffer' },
+    path = { 'dir', 'dir_in_path', 'file', 'file_in_path' },
+  },
 }
 -- spellchecker:on
