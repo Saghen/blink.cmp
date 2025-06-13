feb34a93a2b36afdebc011b8d15a52288beeed98
Dmytro Meleshko <dmytro.meleshko@gmail.com>
2025-06-13 04:28:44 +0200

    fix: skip useless redraws when not running in Neovide (#1903)
    Co-authored-by: Liam Dyer <liamcdyer@gmail.com>

--- lua/blink/cmp/lib/utils.lua
+++ lua/blink/cmp/lib/utils.lua
@@ -163,6 +163,28 @@ function utils.with_no_autocmds(cb)
   return result_or_err
 end
 
+--- Disable redraw in neovide for the duration of the callback
+--- Useful for preventing the cursor from jumping to the top left during `vim.fn.complete`
+--- @generic T
+--- @param fn fun(): T
+--- @return T
+function utils.defer_neovide_redraw(fn)
+  -- don't do anything special when not running inside neovide
+  if not _G.neovide or not neovide.enable_redraw or not neovide.disable_redraw then return fn() end
+
+  neovide.disable_redraw()
+
+  local success, result = pcall(fn)
+
+  -- make sure that the screen is updated and the mouse cursor returned to the right position before re-enabling redrawing
+  pcall(vim.api.nvim__redraw, { cursor = true, flush = true })
+
+  neovide.enable_redraw()
+
+  if not success then error(result) end
+  return result
+end
+
 ---@type boolean Have we passed UIEnter?
 local _ui_entered = vim.v.vim_did_enter == 1 -- technically for VimEnter, but should be good enough for when we're lazy loaded
 ---@type function[] List of notifications.
