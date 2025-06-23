d1e35b05ea9048618aa071937819091a2526c63c
Soifou <fleur.fr@gmail.com>
2025-06-23 23:08:12 +0200

    feat(cmdline): autoshow for cmdwin

--- lua/blink/cmp/config/modes/cmdline.lua
+++ lua/blink/cmp/config/modes/cmdline.lua
@@ -9,7 +9,7 @@ local cmdline = {
     completion = {
       trigger = { show_on_blocked_trigger_characters = {}, show_on_x_blocked_trigger_characters = {} },
       list = { selection = { preselect = true, auto_insert = true } },
-      menu = { auto_show = false },
+      menu = { auto_show = function(ctx, _) return ctx.mode == 'cmdwin' end },
       ghost_text = { enabled = true },
     },
   },
