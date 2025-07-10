a946054f679dcbf60dcf807817cee7bcda05ec6e
Sebastian Lyng Johansen <sebastian@lyngjohansen.com>
2025-07-10 16:56:09 +0200

    fix(winborder): handle custom border chars (#1984)

--- lua/blink/cmp/lib/window/utils.lua
+++ lua/blink/cmp/lib/window/utils.lua
@@ -7,8 +7,11 @@ function utils.pick_border(border, default)
   if border ~= nil then return border end
 
   -- On neovim 0.11+, use the vim.o.winborder option by default
-  local has_winborder, winborder = pcall(function() return vim.o.winborder end)
-  if has_winborder and winborder ~= '' then return winborder end
+  -- Use `vim.opt.winborder:get()` to handle custom border characters
+  if vim.fn.exists('&winborder') == 1 and vim.o.winborder ~= '' then
+    local winborder = vim.opt.winborder:get()
+    return #winborder == 1 and winborder[1] or winborder
+  end
 
   return default or 'none'
 end
