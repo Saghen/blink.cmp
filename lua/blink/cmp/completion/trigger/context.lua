cdd9cb6c5733e369288c5dd746dd42de52aba77c
Liam Dyer <liamcdyer@gmail.com>
2025-07-21 11:36:09 -0400

    feat: additional context for terminal commands

--- lua/blink/cmp/completion/trigger/context.lua
+++ lua/blink/cmp/completion/trigger/context.lua
@@ -12,7 +12,7 @@
 --- @field bufnr number
 --- @field cursor number[]
 --- @field line string
---- @field terminal_command string|nil
+--- @field term blink.cmp.ContextTerm
 --- @field bounds blink.cmp.ContextBounds
 --- @field trigger blink.cmp.ContextTrigger
 --- @field providers string[]
@@ -27,13 +27,21 @@
 --- @field set_cursor fun(cursor: number[])
 --- @field get_line fun(num?: number): string
 --- @field get_bounds fun(range: blink.cmp.CompletionKeywordRange): blink.cmp.ContextBounds
---- @field get_terminal_command fun(): string|nil
+--- @field get_term_command fun(): blink.cmp.ContextTermCommand?
 
 --- @class blink.cmp.ContextTrigger
 --- @field initial_kind blink.cmp.CompletionTriggerKind The trigger kind when the context was first created
 --- @field initial_character? string The trigger character when initial_kind == 'trigger_character'
 --- @field kind blink.cmp.CompletionTriggerKind The current trigger kind
 --- @field character? string The trigger character when kind == 'trigger_character'
+
+--- @class blink.cmp.ContextTerm
+--- @field command blink.cmp.ContextTermCommand
+
+--- @class blink.cmp.ContextTermCommand
+--- @field found_escape_code boolean Whether the FTCS_COMMAND_START escape sequence was found when querying for the command on the current line. This will always be false when the cursor isn't in a prompt, such as when a command is running.
+--- @field text string The command in the current line, without the shell prompt if found_escape_code = true, up to the cursor. Note that for multiline commands, it will always provide you with the content of the last line. This is because there is no way to distinguish the starting point of a single line command from a multiline one using terminal escape sequences
+--- @field start_col number 0-indexed column of the command in the current line, or 0 if the terminal or shell does not support the FTCS_COMMAND_START escape sequence
 
 --- @class blink.cmp.ContextOpts
 --- @field id number
@@ -58,7 +66,7 @@ function context.new(opts)
     bufnr = vim.api.nvim_get_current_buf(),
     cursor = cursor,
     line = line,
-    terminal_command = context.get_terminal_command(),
+    term = { command = context.get_term_command() },
     bounds = context.get_bounds('full'),
     trigger = {
       initial_kind = opts.initial_trigger_kind,
@@ -143,11 +151,17 @@ function context.get_bounds(range)
 end
 
 --- Get the terminal command in the current line without the shell prompt.
---- Caution: this does not support for multiline commands.
 ---
---- @return string|nil
-function context.get_terminal_command()
-  if context.get_mode() ~= 'term' then return nil end
+--- If the terminal or shell does not support the FTCS_COMMAND_START escape sequence,
+--- it will return the full line text, up to the cursor.
+---
+--- In the case of multiline commands, it will always provide you with the content
+--- of the last line. This is because there is no way to distinguish the starting point of a
+--- single line command from a multiline one using terminal escape sequences
+---
+--- @return blink.cmp.ContextTermCommand?
+function context.get_term_command()
+  if context.get_mode() ~= 'term' then return end
 
   local cursor = context.get_cursor()
   local cursor_row = cursor[1]
@@ -165,11 +179,19 @@ function context.get_terminal_command()
   --- If we find no mark for the start of the terminal command the terminal or shell
   --- probably does not support the FTCS_COMMAND_START escape sequence. The best effort
   --- we can do here is to return the full line text.
-  if #extmarks < 1 then return line end
+  if #extmarks < 1 then return {
+    found_escape_code = false,
+    text = line,
+    start_col = 0,
+  } end
 
   local command_start_mark = extmarks[1]
   local command_start_col = command_start_mark[3] + 1
-  return string.sub(line, command_start_col, string.len(line))
+  return {
+    found_escape_code = true,
+    text = string.sub(line, command_start_col, string.len(line)),
+    start_col = command_start_col - 1,
+  }
 end
 
 return context
