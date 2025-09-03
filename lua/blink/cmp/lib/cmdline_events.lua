6323a6ddb191323904557dc9b545f309ea2b0e12
François Fleur <fleur.fr@gmail.com>
2025-09-03 23:08:10 +0200

    perf(cmdline): reduce spurious cursor and char events (#1953)
    * perf(cmdline): reduce spurious cursor and char events
    
    * feat: change burst threshold from 50ms to 2ms
    
    ---------
    
    Co-authored-by: Liam Dyer <liamcdyer@gmail.com>

--- lua/blink/cmp/lib/cmdline_events.lua
+++ lua/blink/cmp/lib/cmdline_events.lua
@@ -20,33 +20,29 @@ function cmdline_events.new()
   return setmetatable({
     ignore_next_text_changed = false,
     ignore_next_cursor_moved = false,
-  }, { __index = cmdline_events })
+  }, { __index = cmdline_events }) --[[@as blink.cmp.CmdlineEvents]]
 end
 
 function cmdline_events:listen(opts)
   -- TextChanged
   local on_changed = function(key) opts.on_char_added(key, false) end
 
-  -- We handle backspace as a special case, because the text will have changed
-  -- but we still want to fire the CursorMoved event, and not the TextChanged event
-  local did_backspace = false
+  local last_move_time, pending_key
   local is_change_queued = false
-  local pending_key = nil
   vim.on_key(function(raw_key, escaped_key)
     if vim.api.nvim_get_mode().mode ~= 'c' then return end
 
     -- ignore if it's a special key
     -- FIXME: odd behavior when escaped_key has multiple keycodes, i.e. by pressing <C-p> and then "t"
     local key = vim.fn.keytrans(escaped_key)
-    if key == '<BS>' and not is_change_queued then did_backspace = true end
     if key:sub(1, 1) == '<' and key:sub(#key, #key) == '>' and raw_key ~= ' ' then return end
     if key == '' then return end
 
+    last_move_time = vim.loop.hrtime() / 1e6
     pending_key = raw_key
 
     if not is_change_queued then
       is_change_queued = true
-      did_backspace = false
       vim.schedule(function()
         on_changed(pending_key)
         is_change_queued = false
@@ -54,6 +50,19 @@ function cmdline_events:listen(opts)
       end)
     end
   end)
+
+  -- Abbreviations and other automated features can cause rapid, repeated cursor movements
+  -- (a "burst") that are not intentional user actions. To avoid reacting to these artificial
+  -- movements in CursorMovedC, we detect bursts by measuring the time between moves.
+  -- If two cursor moves occur within a short threshold (burst_threshold_ms), we treat them
+  -- as part of a burst and ignore them.
+  local burst_threshold_ms = 2
+  local function is_burst_move()
+    local current_time = vim.loop.hrtime() / 1e6
+    local is_burst = last_move_time and (current_time - last_move_time) < burst_threshold_ms
+    last_move_time = current_time
+    return is_burst or false
+  end
 
   -- CursorMoved
   if vim.fn.has('nvim-0.11') == 1 then
@@ -66,7 +75,7 @@ function cmdline_events:listen(opts)
 
         if is_change_queued then return end
 
-        opts.on_cursor_moved('CursorMoved', is_ignored)
+        if not is_burst_move() then opts.on_cursor_moved('CursorMoved', is_ignored) end
       end,
     })
 
@@ -83,23 +92,18 @@ function cmdline_events:listen(opts)
 
       local current_cmdline = vim.fn.getcmdline()
       local current_cursor = vim.fn.getcmdpos()
-
-      -- Detect changes in command line or cursor position
-      local cmdline_changed = current_cmdline ~= previous_cmdline
       local cursor_changed = current_cursor ~= previous_cursor
+
+      -- Fire on_cursor_moved if cursor changed or destructive edits (<BS>, <C-W> or <C-u>)
+      if cursor_changed and #current_cmdline < #previous_cmdline then
+        local is_ignored = self.ignore_next_cursor_moved
+        self.ignore_next_cursor_moved = false
+        if is_change_queued then return end
+        opts.on_cursor_moved('CursorMoved', is_ignored)
+      end
 
       previous_cmdline = current_cmdline
       previous_cursor = current_cursor
-
-      -- Fire on_cursor_moved if either text or cursor changed
-      if cursor_changed or (cmdline_changed and not did_backspace) then
-        did_backspace = false
-
-        local is_ignored = self.ignore_next_cursor_moved
-        self.ignore_next_cursor_moved = false
-
-        opts.on_cursor_moved('CursorMoved', is_ignored)
-      end
     end)
     vim.api.nvim_create_autocmd('CmdlineEnter', {
       callback = function()
